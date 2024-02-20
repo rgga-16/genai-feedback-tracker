693c0a7d0f6fe3cb9c2397edb5eb605962d31d42
rgga16 <rgga16@gmail.com>
2024-02-20 16:58:00 +0900

    added util functions

---
+++ utils.py
@@ -0,0 +1,22 @@
+import os, shutil
+
+def makedir(dir_path):
+    try:
+        os.makedirs(dir_path)
+    except FileExistsError:
+        pass
+
+def emptydir(dir,delete_dirs=False):
+    # loop through all the files and subdirectories in the directory
+    for filename in os.listdir(dir):
+        file_path = os.path.join(dir, filename)
+        try:
+            if os.path.isfile(file_path):
+                # remove the file
+                print(f"{file_path} deleted")
+                os.remove(file_path)
+            elif delete_dirs and os.path.isdir(file_path):
+                # remove the subdirectory and its contents
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
