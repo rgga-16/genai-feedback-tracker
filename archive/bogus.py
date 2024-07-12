f4435b79dd0c2551459cf0548062c252de8b14ba
rgga16 <rgga16@gmail.com>
2024-07-12 13:11:28 +0900

    archived

---
+++ archive/bogus.py
@@ -0,0 +1,33 @@
+from openai import OpenAI
+import base64
+
+client = OpenAI()
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+  
+base64_image= encode_image("./screenshot.png")
+
+response = client.chat.completions.create(
+  model="gpt-4o",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {"type": "text", "text": "What’s in this image?"},
+        {
+          "type": "image_url",
+          "image_url": {
+            "url":  f"data:image/jpeg;base64,{base64_image}"
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=300,
+)
+
+print(response.choices[0].message.content)
+pass
