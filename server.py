b0d6c9c7d03e28bc99f1da260bf3b9ec6aeedf54
rgga16 <rgga16@gmail.com>
2024-08-09 15:28:09 +0900

    small change

--- server.py
+++ server.py
@@ -397,7 +397,7 @@ def message_chatbot():
 
 
     n_rows = transcript_db.shape[0]
-    top_n = 5
+    top_n = 0.2*n_rows
     transcript_excerpts, relatednesses = strings_ranked_by_relatedness(
         message, 
         transcript_db,
@@ -406,7 +406,7 @@ def message_chatbot():
     transcript_excerpts_string = "\n".join(transcript_excerpts)
 
     n_rows = document_db.shape[0]
-    top_n = 5
+    top_n = 0.2*n_rows
     document_excerpts, relatednesses = strings_ranked_by_relatedness(
         message, 
         document_db,
@@ -426,7 +426,7 @@ def message_chatbot():
     
     Here are the top related document excerpts:
     {document_excerpts_string}
-    If your answer is based on a specific document excerpt, please mention the page number of the excerpt and the reference (e.g. book) name.
+    If your answer is based on a specific document excerpt, please mention the page number of the excerpt and the name of the document (e.g. book).
 
     If the query is not related to any of the transcripts or documents, answer the query as best as possible based on your own knowledge as an interior design expert.
     """
@@ -435,7 +435,7 @@ def message_chatbot():
     if image_data:
         visual_context = f"\nMoreover, the user attached an image for visual context. Please use the image as context and to help with your answer."
 
-    last_instruction="\n\nTogether with the contexts retrieved, and the visual context (if any), please respond concisely the query. Please answer in 1-3 sentences at most."
+    last_instruction="\n\nTogether with the contexts retrieved, and the visual context (if any), please respond to the query."
     
     full_instruction = instruction + visual_context + contexts + last_instruction
 
